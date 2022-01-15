6f55b000d5fb3d9e52d688864d082c0c829552d9
Isaac Frank <codingwithisaac@gmail.com>
2022-01-15 17:17:43 +0100

    feat: --

--- run.js
+++
@@ -1,2 +0,0 @@
-"npx hardhat verify <> --contract contracts/ENHANCE.sol:ENHANCE --libraries libraries.js --network bsc_main";
-"npx hardhat verify <> <> --contract contracts/DividendTracker.sol:DividendTracker --libraries libraries.js --network bsc_main"
